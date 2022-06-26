93c211d0f6a6c8c5cdb74479d5f592211178eb32
Philipp Schermann <PhiSch19@outlook.com>
2022-06-26 10:30:34 +0200

    Initial commit

---
+++ src/test/java/at/phisch/maintenancemonitor/MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.phisch.maintenancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
